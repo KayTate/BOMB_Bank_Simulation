441ebac6c2ffd619b0a389aaf4965ea1c96f50c6
Olivia <olivia.asemota12@gmail.com>
2018-11-30 18:30:38 -0600

    Complaint to Manager
    Added complaint to manager file

---
+++ complaint.py
@@ -0,0 +1,34 @@
+def complaint_to_man():
+    print('''
+    Finding the service at the Bank of Mashed Beats less than desireable, you decide to speak with the manager of B.O.M.B.
+    You glare at the employee who has given you such rudimentary service and demand that you speak to the manager.
+    
+    The employee stares back at you blankly. They then slowly open their mouth and explain, "Unfortunately, we do not have a head manager at this time. Our last one just recently decided to take all of his savings a plan a family vacation to the plantet Mercury. Yes, I know it's quite strange, but my attempt to persuade him failed miserably"
+
+
+    ''')
+    q1 = input('''
+    You stare back at the teller confused as all fuck. 
+    You have three options:
+    1) You could tell the teller that they are the new acting manager and flip them off confidently.
+    2) You could storm into the \'Manager\' office on your left and see the truth for yourself
+    3) You could turn and walk away. This bank is filled with lunatic shitheads and you\'re too much of a boss (a poor boss, but a boss still) to take this bull.
+
+    ''')
+
+    if q1 == '1':
+        print('''
+        The teller raises their eyebrow at you as if your shorter-than-average middle phalange is going to actually make your declaration and their \'new\' position any more real.
+        "Yeah, listen...", the teller starts "I don\'t know what\'s up wiht you and trying to make me angry but I highkey don\'t have a soul. I work at a fucking bank for christ\'s sake.
+        BUT! I could use the promotion and our old manager was an asshole anyway, sooooooooo you\'re looking a the new acting manager of B.O.M.B.
+        They wink, feeling alot more proud of themselves. You can\'t help but feel betryaed because you GAVE them that position, who are they too feel proud about something as illegitmet as getting a job from a customer....you try not to ponder the hyprocity in that statement for too long.
+
+        The new manager gets up from the teller booth and walks towards a door with the word \'Manager\' on it. Hmmmmm, maybe you should have just gone over there first, but it doesn\'t matter at this point. 
+        They are gone for approximately 10 minutes before they walk out with a new golden badge with their name on it.
+        I guess the manager is just special like that....
+        They come back to 
+  ''')
+
+    elif q1 == '2':
+
+    else:
